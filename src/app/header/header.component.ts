806b773eee26f548eb13620161d580d7eaebeb00
Daniel Machin Quintana <135682721+Dmachin7@users.noreply.github.com>
2024-02-20 12:12:35 -0500

    made changes to mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,38 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit  {
   isScrolled: Boolean = false;
+  show: String = 'dontShowNav';
+  display: String = "show"
+
+  showMobileNav = () =>  {
+    switch (this.show) {
+      case "dontShowNav":
+        this.display = "hide"
+        this.show = "showNav"
+        break
+      case "showNav":
+        this.display = "show"
+        this.show = "dontShowNav"
+        break
+    }
+  }
+
 
   @HostListener('window:scroll', [])
   onWindowScrol() {
     this.isScrolled = window.scrollY > 400;
+  }
+
+ 
+
+  ngOnInit(): void {
+      
   }
 
 }
